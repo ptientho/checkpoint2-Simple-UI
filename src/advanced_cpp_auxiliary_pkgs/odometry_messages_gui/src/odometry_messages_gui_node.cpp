8c682f8d4f0888816ff8bc6c53d744a478946bf3
MysterP <78469954+ptientho@users.noreply.github.com>
2023-08-27 19:19:28 -0400

    Delete src directory

--- src/advanced_cpp_auxiliary_pkgs/odometry_messages_gui/src/odometry_messages_gui_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "odometry_messages_gui/odometry_messages_gui.h"
-#include <ros/ros.h>
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "odometry_messages_gui");
-  CVUIROSOdomSubscriber odometry_messages_gui;
-  odometry_messages_gui.run();
-  return 0;
-}
--- src/advanced_cpp_auxiliary_pkgs/show_received_messages/src/show_received_messages_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "show_received_messages/show_received_messages.h"
-#include <ros/ros.h>
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "show_received_messages");
-  CVUIROSSubscriber show_received_messages;
-  show_received_messages.run();
-  return 0;
-}
